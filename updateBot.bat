e23e3a98f9651a04ce99cf6436a571678bcb41a3
Underforest <neovisatoons@gmail.com>
2016-12-17 22:24:58 -0800

    Update updateBot.bat

--- updateBot.bat
+++ updateBot.bat
@@ -15,14 +15,14 @@ echo Press ENTER and the download will start on your favorite browser.
 echo Once done, follow the instructions.
 PAUSE>NUL
 start "" https://github.com/Underforest/hifumibot/archive/master.zip
-echo --------------------------------------------------------
+echo ----------------------------------------------------------------
 echo INSTRUCTIONS
 echo Once the download is finished, do the following!
 echo 1. Open master.zip
-echo 2. Extract the "hifumibot-master" folder content (Replace all
-echo files with new one, it shouldn't affect the bot)
+echo 2. Extract the "hifumibot-master" folder content (Replace
+echo all files with new one, it shouldn't affect the bot)
 echo 3. Start the bot and enjoy!
-echo --------------------------------------------------------
+echo ----------------------------------------------------------------
 echo.
 echo Press any key to exit.
 PAUSE>NUL
@@ -35,15 +35,15 @@ echo Please don't close this window or disconnect from the network.
 echo Stay here to know when the download finishes and follow the instructions.
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/Underforest/hifumibot/archive/master.zip', 'master.zip')"
 echo Download finished!
-echo --------------------------------------------------------
+echo ----------------------------------------------------------------
 echo INSTRUCTIONS
 echo Once the download is finished, do the following!
 echo 1. Verify that there's a "master.zip" file on your
 echo workplace. If so, open master.zip
-echo 2. Extract the "hifumibot-master" folder content (Replace all
-echo files with new one, it shouldn't affect the bot)
+echo 2. Extract the "hifumibot-master" folder content (Replace
+echo all files with new one, it shouldn't affect the bot)
 echo 3. Start the bot and enjoy!
-echo --------------------------------------------------------
+echo ----------------------------------------------------------------
 echo.
 echo Press any key to exit.
 PAUSE>NUL
